6573cd621d084b61fe138c1df52a5b2eb918c592
Tung_HD <truongquangtung2004@gmail.com>
2025-07-04 20:35:27 +0700

    d
    d

--- HE_THONG_BAN_XE/Form1.Designer.cs
+++ HE_THONG_BAN_XE/Form1.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1289, 723);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainForm";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
--- HE_THONG_BAN_XE/Form1.cs
+++ HE_THONG_BAN_XE/Form1.cs
@@ -6,5 +6,13 @@ namespace HE_THONG_BAN_XE
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chào mừng bạn đến với hệ thống hé hé",
+                "Thông báo",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
     }
 }
