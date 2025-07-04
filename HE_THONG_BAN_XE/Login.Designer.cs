e9b781e13a3eae134a4478f0742cfe6dfb64bca9
PNKDuy123 <khuongduy2017sl@gmail.com>
2025-07-05 00:03:19 +0700

    radio

--- HE_THONG_BAN_XE/Login.Designer.cs
+++ HE_THONG_BAN_XE/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HE_THONG_BAN_XE
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         ///  Required designer variable.
@@ -30,10 +30,12 @@
         {
             label1 = new Label();
             label2 = new Label();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            txt_Name = new TextBox();
+            txt_Pass = new TextBox();
             button1 = new Button();
             button3 = new Button();
+            radioButton_QuanLy = new RadioButton();
+            radioButton_NhanVien = new RadioButton();
             SuspendLayout();
             // 
             // label1
@@ -54,23 +56,23 @@
             label2.TabIndex = 1;
             label2.Text = "Mật khẩu :";
             // 
-            // textBox1
+            // txt_Name
             // 
-            textBox1.Location = new Point(156, 23);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(211, 27);
-            textBox1.TabIndex = 2;
+            txt_Name.Location = new Point(156, 23);
+            txt_Name.Name = "txt_Name";
+            txt_Name.Size = new Size(211, 27);
+            txt_Name.TabIndex = 2;
             // 
-            // textBox2
+            // txt_Pass
             // 
-            textBox2.Location = new Point(156, 70);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(211, 27);
-            textBox2.TabIndex = 3;
+            txt_Pass.Location = new Point(156, 70);
+            txt_Pass.Name = "txt_Pass";
+            txt_Pass.Size = new Size(211, 27);
+            txt_Pass.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(25, 119);
+            button1.Location = new Point(25, 169);
             button1.Name = "button1";
             button1.Size = new Size(150, 29);
             button1.TabIndex = 4;
@@ -79,27 +81,51 @@
             // 
             // button3
             // 
-            button3.Location = new Point(206, 119);
+            button3.Location = new Point(202, 169);
             button3.Name = "button3";
             button3.Size = new Size(150, 29);
             button3.TabIndex = 6;
             button3.Text = "Thoát";
             button3.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // radioButton_QuanLy
+            // 
+            radioButton_QuanLy.AutoSize = true;
+            radioButton_QuanLy.Location = new Point(234, 126);
+            radioButton_QuanLy.Name = "radioButton_QuanLy";
+            radioButton_QuanLy.Size = new Size(82, 24);
+            radioButton_QuanLy.TabIndex = 7;
+            radioButton_QuanLy.TabStop = true;
+            radioButton_QuanLy.Text = "Quản Lý";
+            radioButton_QuanLy.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_NhanVien
+            // 
+            radioButton_NhanVien.AutoSize = true;
+            radioButton_NhanVien.Location = new Point(60, 126);
+            radioButton_NhanVien.Name = "radioButton_NhanVien";
+            radioButton_NhanVien.Size = new Size(96, 24);
+            radioButton_NhanVien.TabIndex = 8;
+            radioButton_NhanVien.TabStop = true;
+            radioButton_NhanVien.Text = "Nhân viên";
+            radioButton_NhanVien.UseVisualStyleBackColor = true;
+            // 
+            // Login
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(379, 172);
+            ClientSize = new Size(379, 224);
+            Controls.Add(radioButton_NhanVien);
+            Controls.Add(radioButton_QuanLy);
             Controls.Add(button3);
             Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(txt_Pass);
+            Controls.Add(txt_Name);
             Controls.Add(label2);
             Controls.Add(label1);
             MaximizeBox = false;
             MinimizeBox = false;
-            Name = "Form1";
+            Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Đăng nhập";
             Load += Form1_Load;
@@ -111,9 +137,11 @@
 
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox txt_Name;
+        private TextBox txt_Pass;
         private Button button1;
         private Button button3;
+        private RadioButton radioButton_QuanLy;
+        private RadioButton radioButton_NhanVien;
     }
 }
--- HE_THONG_BAN_XE/Login.cs
+++ HE_THONG_BAN_XE/Login.cs
@@ -1,8 +1,8 @@
 namespace HE_THONG_BAN_XE
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
