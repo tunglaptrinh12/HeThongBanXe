28deafd5c9a4a7a58e79d7a07f9723c84bf0480a
PNKDuy123 <khuongduy2017sl@gmail.com>
2025-07-05 11:25:24 +0700

    thêm 3 dữ liệu cho 3 bảng

---
+++ HE_THONG_BAN_XE/model/HoaDon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HE_THONG_BAN_XE.model
+{
+    internal class HoaDon
+    {
+    }
+}
---
+++ HE_THONG_BAN_XE/model/KhachHangDaKhuyenMai.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HE_THONG_BAN_XE.model
+{
+    [Table("CSDL_KhachHangDaKhuyenMai")]
+    internal class KhachHangDaKhuyenMai
+    {
+        [Key]
+        [Required,StringLength(50)]
+        public String? MaKhuyenMai { get; set; }
+        [Required, StringLength(50)]
+        public String? MaKH { get; set; }
+        [Required]
+        public DateTime NgayApDung { get; set; }
+        [Required]
+        public Decimal GiaTriKhuyenMai { get; set; } // Phần trăm khuyến mãi
+        [Required,StringLength(255)]
+        public String? GhiChu { get; set; } // Mô tả về chương trình khuyến mãi
+
+    }
+}
