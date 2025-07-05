d6823183eb2be4556f023605f33480b2332bd86a
PNKDuy123 <khuongduy2017sl@gmail.com>
2025-07-05 11:41:23 +0700

    Xe va Hoa Don

--- HE_THONG_BAN_XE/model/HoaDon.cs
+++ HE_THONG_BAN_XE/model/HoaDon.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HE_THONG_BAN_XE.model
 {
+    [Table("CSDL_HoaDon")]
     internal class HoaDon
     {
+        [Key]
+        [Required]
+        [MaxLength(20)]
+        public String? MaHD { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public String? MaKH { get; set; }
+
+        [Required, StringLength(10)]
+        public String? MaXe { get; set; }
+
+        public DateTime NgayLayHD { get; set; }
+
+        public Decimal ThanhTien { get; set; }
+        //m
+
     }
+
 }
--- HE_THONG_BAN_XE/model/Xe.cs
+++ HE_THONG_BAN_XE/model/Xe.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HE_THONG_BAN_XE.model
-{
+{   
+    [Table("CSDL_Xe")]
     internal class Xe
     {
+        [Key]
+        [Required,StringLength(10)]
+        public String? MaXe { get; set; }
+        [Required, StringLength(100)]
+        public String? TenXe { get; set; }
+        [Required, StringLength(50)]
+        public String? HangXe { get; set; }
+        [Required, StringLength(50)]
+        public String? LoaiXe { get; set; }
+        [Required, StringLength(50)]
+        public String? MauSac { get; set; }
+        [Required]
+        public int SoChoNgoi { get; set; }
+        [Required]
+        public DateTime NamSanXuat { get; set; }
+        [Required, StringLength(20)]
+        public String? BienSo { get; set; }
+        [Required, StringLength(10)]
+        public String? MoiCu { get; set; } 
+        [Required]
+        public Decimal GiaBan { get; set; }
+        [Required, StringLength(20)]
+        public String? TrangThai { get; set; } // Mới, Đã bán, Đang bảo trì, Hết hàng
+
+        //h
+
     }
 }
