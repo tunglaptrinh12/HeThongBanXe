84191211d7cf7968972277bb37e5ef3a00689028
PNKDuy123 <khuongduy2017sl@gmail.com>
2025-07-24 15:52:02 +0700

    Trang Báo Cáo

--- HE_THONG_BAN_XE/ControlHeThong/BaoCaoControl.Designer.cs
+++ HE_THONG_BAN_XE/ControlHeThong/BaoCaoControl.Designer.cs
@@ -29,37 +29,52 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            dateTimePicker1 = new DateTimePicker();
+            dtNgay = new DateTimePicker();
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
             label5 = new Label();
             label6 = new Label();
             panel1 = new Panel();
+            lblXeBan = new Label();
             panel2 = new Panel();
+            lblTonKho = new Label();
             panel3 = new Panel();
+            lblKhachHang = new Label();
             panel4 = new Panel();
+            lblNhanVien = new Label();
             panel5 = new Panel();
+            lblDoanhThu = new Label();
             label7 = new Label();
+            dgvXeBan = new DataGridView();
+            label8 = new Label();
+            btnTaoBaoCao = new Button();
+            btnXuatBaoCao = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvXeBan).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Times New Roman", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.DarkBlue;
-            label1.Location = new Point(20, 15);
+            label1.Location = new Point(20, 27);
             label1.Name = "label1";
-            label1.Size = new Size(422, 32);
+            label1.Size = new Size(512, 38);
             label1.TabIndex = 0;
             label1.Text = "Báo cáo kinh doanh cửa hàng ô tô";
             // 
-            // dateTimePicker1
+            // dtNgay
             // 
-            dateTimePicker1.Location = new Point(839, 25);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(250, 27);
-            dateTimePicker1.TabIndex = 1;
+            dtNgay.Location = new Point(839, 25);
+            dtNgay.Name = "dtNgay";
+            dtNgay.Size = new Size(250, 27);
+            dtNgay.TabIndex = 1;
             // 
             // label2
             // 
@@ -76,7 +91,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Times New Roman", 13.8F);
-            label3.Location = new Point(52, 69);
+            label3.Location = new Point(19, 119);
             label3.Name = "label3";
             label3.Size = new Size(122, 26);
             label3.TabIndex = 0;
@@ -86,7 +101,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Times New Roman", 13.8F);
-            label4.Location = new Point(459, 69);
+            label4.Location = new Point(19, 460);
             label4.Name = "label4";
             label4.Size = new Size(186, 26);
             label4.TabIndex = 1;
@@ -96,7 +111,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Times New Roman", 13.8F);
-            label5.Location = new Point(459, 279);
+            label5.Location = new Point(19, 568);
             label5.Name = "label5";
             label5.Size = new Size(165, 26);
             label5.TabIndex = 2;
@@ -106,71 +121,175 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Times New Roman", 13.8F);
-            label6.Location = new Point(52, 493);
+            label6.Location = new Point(19, 348);
             label6.Name = "label6";
             label6.Size = new Size(186, 26);
             label6.TabIndex = 3;
             label6.Text = "Tổng khách hàng: ";
+            label6.Click += label6_Click;
             // 
             // panel1
             // 
             panel1.BackColor = Color.PowderBlue;
-            panel1.Location = new Point(52, 308);
+            panel1.Controls.Add(lblXeBan);
+            panel1.Location = new Point(19, 263);
             panel1.Name = "panel1";
-            panel1.Size = new Size(357, 153);
+            panel1.Size = new Size(208, 63);
             panel1.TabIndex = 4;
             panel1.Paint += panel1_Paint;
+            // 
+            // lblXeBan
+            // 
+            lblXeBan.AutoSize = true;
+            lblXeBan.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblXeBan.Location = new Point(4, 20);
+            lblXeBan.Name = "lblXeBan";
+            lblXeBan.Size = new Size(47, 25);
+            lblXeBan.TabIndex = 1;
+            lblXeBan.Text = "Null";
             // 
             // panel2
             // 
             panel2.BackColor = Color.PowderBlue;
-            panel2.Location = new Point(459, 94);
+            panel2.Controls.Add(lblTonKho);
+            panel2.Location = new Point(19, 489);
             panel2.Name = "panel2";
-            panel2.Size = new Size(357, 153);
+            panel2.Size = new Size(208, 63);
             panel2.TabIndex = 5;
+            // 
+            // lblTonKho
+            // 
+            lblTonKho.AutoSize = true;
+            lblTonKho.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTonKho.Location = new Point(4, 20);
+            lblTonKho.Name = "lblTonKho";
+            lblTonKho.Size = new Size(47, 25);
+            lblTonKho.TabIndex = 1;
+            lblTonKho.Text = "Null";
             // 
             // panel3
             // 
             panel3.BackColor = Color.PowderBlue;
-            panel3.Location = new Point(52, 522);
+            panel3.Controls.Add(lblKhachHang);
+            panel3.Location = new Point(19, 377);
             panel3.Name = "panel3";
-            panel3.Size = new Size(357, 153);
+            panel3.Size = new Size(208, 63);
             panel3.TabIndex = 6;
             panel3.Paint += panel3_Paint;
+            // 
+            // lblKhachHang
+            // 
+            lblKhachHang.AutoSize = true;
+            lblKhachHang.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblKhachHang.Location = new Point(4, 22);
+            lblKhachHang.Name = "lblKhachHang";
+            lblKhachHang.Size = new Size(47, 25);
+            lblKhachHang.TabIndex = 1;
+            lblKhachHang.Text = "Null";
             // 
             // panel4
             // 
             panel4.BackColor = Color.PowderBlue;
-            panel4.Location = new Point(459, 308);
+            panel4.Controls.Add(lblNhanVien);
+            panel4.Location = new Point(19, 597);
             panel4.Name = "panel4";
-            panel4.Size = new Size(357, 153);
+            panel4.Size = new Size(208, 63);
             panel4.TabIndex = 7;
+            // 
+            // lblNhanVien
+            // 
+            lblNhanVien.AutoSize = true;
+            lblNhanVien.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNhanVien.Location = new Point(4, 20);
+            lblNhanVien.Name = "lblNhanVien";
+            lblNhanVien.Size = new Size(47, 25);
+            lblNhanVien.TabIndex = 1;
+            lblNhanVien.Text = "Null";
             // 
             // panel5
             // 
             panel5.BackColor = Color.PowderBlue;
-            panel5.Location = new Point(52, 94);
+            panel5.Controls.Add(lblDoanhThu);
+            panel5.Location = new Point(20, 148);
             panel5.Name = "panel5";
-            panel5.Size = new Size(357, 153);
+            panel5.Size = new Size(208, 63);
             panel5.TabIndex = 8;
-            //panel5.Paint += this.panel5_Paint;
+            // 
+            // lblDoanhThu
+            // 
+            lblDoanhThu.AutoSize = true;
+            lblDoanhThu.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDoanhThu.Location = new Point(3, 19);
+            lblDoanhThu.Name = "lblDoanhThu";
+            lblDoanhThu.Size = new Size(47, 25);
+            lblDoanhThu.TabIndex = 0;
+            lblDoanhThu.Text = "Null";
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Times New Roman", 13.8F);
-            label7.Location = new Point(52, 279);
+            label7.Location = new Point(19, 234);
             label7.Name = "label7";
             label7.Size = new Size(219, 26);
             label7.TabIndex = 9;
             label7.Text = "Tổng số xe bán được :";
             label7.Click += label7_Click;
             // 
+            // dgvXeBan
+            // 
+            dgvXeBan.BackgroundColor = Color.FloralWhite;
+            dgvXeBan.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvXeBan.GridColor = SystemColors.HighlightText;
+            dgvXeBan.Location = new Point(274, 148);
+            dgvXeBan.Name = "dgvXeBan";
+            dgvXeBan.RowHeadersWidth = 51;
+            dgvXeBan.Size = new Size(838, 446);
+            dgvXeBan.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(464, 112);
+            label8.Name = "label8";
+            label8.Size = new Size(480, 33);
+            label8.TabIndex = 11;
+            label8.Text = "Bảng báo cáo số lượng xe bán trong ngày ";
+            // 
+            // btnTaoBaoCao
+            // 
+            btnTaoBaoCao.Cursor = Cursors.Hand;
+            btnTaoBaoCao.Font = new Font("Times New Roman", 10.2F);
+            btnTaoBaoCao.Location = new Point(816, 617);
+            btnTaoBaoCao.Name = "btnTaoBaoCao";
+            btnTaoBaoCao.Size = new Size(128, 43);
+            btnTaoBaoCao.TabIndex = 12;
+            btnTaoBaoCao.Text = "Tạo báo cáo\r\n";
+            btnTaoBaoCao.UseVisualStyleBackColor = true;
+            btnTaoBaoCao.Click += btnTaoBaoCao_Click;
+            // 
+            // btnXuatBaoCao
+            // 
+            btnXuatBaoCao.Cursor = Cursors.Hand;
+            btnXuatBaoCao.Font = new Font("Times New Roman", 10.2F);
+            btnXuatBaoCao.Location = new Point(984, 617);
+            btnXuatBaoCao.Name = "btnXuatBaoCao";
+            btnXuatBaoCao.Size = new Size(128, 43);
+            btnXuatBaoCao.TabIndex = 13;
+            btnXuatBaoCao.Text = "Xuất báo cáo";
+            btnXuatBaoCao.UseVisualStyleBackColor = true;
+            btnXuatBaoCao.Click += btnXuatBaoCao_Click;
+            // 
             // BaoCaoControl
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightSkyBlue;
+            Controls.Add(btnXuatBaoCao);
+            Controls.Add(btnTaoBaoCao);
+            Controls.Add(label8);
+            Controls.Add(dgvXeBan);
             Controls.Add(panel2);
             Controls.Add(label7);
             Controls.Add(panel5);
@@ -179,7 +298,7 @@
             Controls.Add(panel3);
             Controls.Add(label5);
             Controls.Add(label2);
-            Controls.Add(dateTimePicker1);
+            Controls.Add(dtNgay);
             Controls.Add(label4);
             Controls.Add(panel1);
             Controls.Add(label1);
@@ -187,6 +306,17 @@
             Name = "BaoCaoControl";
             Size = new Size(1147, 721);
             Load += BaoCaoControl_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvXeBan).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -194,7 +324,7 @@
         #endregion
 
         private Label label1;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dtNgay;
         private Label label2;
         private Label label3;
         private Label label4;
@@ -206,5 +336,14 @@
         private Panel panel4;
         private Panel panel5;
         private Label label7;
+        private DataGridView dgvXeBan;
+        private Label label8;
+        private Button btnTaoBaoCao;
+        private Button btnXuatBaoCao;
+        private Label lblXeBan;
+        private Label lblTonKho;
+        private Label lblKhachHang;
+        private Label lblNhanVien;
+        private Label lblDoanhThu;
     }
 }
--- HE_THONG_BAN_XE/ControlHeThong/BaoCaoControl.cs
+++ HE_THONG_BAN_XE/ControlHeThong/BaoCaoControl.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Spreadsheet;
+using HE_THONG_BAN_XE.Connect;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+
+
 
 namespace HE_THONG_BAN_XE.ControlHeThong
 {
@@ -38,5 +52,128 @@ namespace HE_THONG_BAN_XE.ControlHeThong
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTaoBaoCao_Click(object sender, EventArgs e)
+        {
+            DateTime ngayChon = dtNgay.Value.Date;
+
+            using (var db = new DBNhanVien())
+            {
+                // ✅ DOANH THU TRONG NGÀY
+                var doanhThu = db.hoaDons
+                    .Where(hd => hd.NgaylapHD.HasValue && hd.NgaylapHD.Value.Date == ngayChon)
+                    .Sum(hd => (decimal?)hd.ThanhTien) ?? 0;
+                lblDoanhThu.Text = doanhThu.ToString("N0") + " VNĐ";
+
+                // ✅ TỔNG XE ĐÃ BÁN TRONG NGÀY (theo chi tiết hóa đơn)
+                var xeBan = (from ct in db.chiTietHoaDons
+                             join hd in db.hoaDons on ct.MaHD equals hd.MaHD
+                             where hd.NgaylapHD.HasValue && hd.NgaylapHD.Value.Date == ngayChon
+                             select ct).Count();
+                lblXeBan.Text = xeBan.ToString();
+
+                // ✅ TỔNG KHÁCH HÀNG
+                lblKhachHang.Text = db.khachHangs.Count().ToString();
+
+                // ✅ TỒN KHO (Tổng số lượng xe còn lại)
+                var tonKho = db.xes.Sum(x => (int?)x.SoChoNgoi) ?? 0;
+                lblTonKho.Text = tonKho.ToString();
+
+                // ✅ TỔNG NHÂN VIÊN
+                lblNhanVien.Text = db.nhanViens.Count().ToString();
+
+                // ✅ DỮ LIỆU XE ĐÃ BÁN TRONG NGÀY (dgvXeBan)
+                var danhSachXeBan = from ct in db.chiTietHoaDons
+                                    join hd in db.hoaDons on ct.MaHD equals hd.MaHD
+                                    join xe in db.xes on ct.MaXe equals xe.MaXe
+                                    join kh in db.khachHangs on ct.MaKH equals kh.MaKH
+                                    where hd.NgaylapHD.HasValue && hd.NgaylapHD.Value.Date == ngayChon
+                                    select new
+                                    {
+                                        ct.MaCTHD,
+                                        hd.MaHD,
+                                        TenKhachHang = kh.TenKH,
+                                        TenXe = xe.TenXe,
+                                        ct.DonGia,
+                                        ct.ThanhTien,
+                                        NgayLap = hd.NgaylapHD.Value
+                                    };
+
+                dgvXeBan.DataSource = danhSachXeBan.ToList();
+            }
+
+        }
+
+       
+
+        private void btnXuatBaoCao_Click(object sender, EventArgs e)
+        {
+            string filePath = "BaoCao_" + dtNgay.Value.ToString("yyyyMMdd_HHmmss") + ".pdf";
+
+            using (PdfWriter writer = new PdfWriter(filePath))
+            {
+                using (PdfDocument pdf = new PdfDocument(writer))
+                {
+                    Document doc = new Document(pdf);
+
+                    // Title
+                    Paragraph title = new Paragraph("BÁO CÁO BÁN HÀNG")
+                        .SetTextAlignment(TextAlignment.CENTER)
+                        .SetFontSize(20);
+
+                    doc.Add(title);
+
+                    doc.Add(new Paragraph("\n"));
+
+                    // Ngày tạo báo cáo
+                    string selectedDate = dtNgay.Value.ToString("dd/MM/yyyy");
+                    doc.Add(new Paragraph("Ngày: " + selectedDate));
+
+                    // Doanh thu
+                    doc.Add(new Paragraph("Doanh thu trong ngày: " + lblDoanhThu.Text));
+                    doc.Add(new Paragraph("Tổng số xe bán được: " + lblXeBan.Text));
+                    doc.Add(new Paragraph("Tổng khách hàng: " + lblKhachHang.Text));
+                    doc.Add(new Paragraph("Sản lượng tồn kho: " + lblTonKho.Text));
+                    doc.Add(new Paragraph("Tổng nhân viên: " + lblNhanVien.Text));
+
+                    doc.Add(new Paragraph("\nDanh sách xe đã bán trong ngày:\n"));
+
+                    // Bảng từ DataGridView
+                    iText.Layout.Element.Table table = new iText.Layout.Element.Table(dgvXeBan.Columns.Count);
+
+                    // Thêm header
+                    foreach (DataGridViewColumn col in dgvXeBan.Columns)
+                    {
+                        table.AddHeaderCell(new iText.Layout.Element.Cell().Add(new Paragraph(col.HeaderText)));
+                    }
+
+                    // Thêm dữ liệu
+                    foreach (DataGridViewRow row in dgvXeBan.Rows)
+                    {
+                        if (!row.IsNewRow)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                table.AddCell(new iText.Layout.Element.Cell().Add(new Paragraph(cell.Value?.ToString() ?? "")));
+                            }
+                        }
+                    }
+
+                    doc.Add(table);
+
+                    doc.Close();
+                }
+            }
+
+            MessageBox.Show("Xuất báo cáo thành công!\nFile lưu tại: " + Path.GetFullPath(filePath),
+                            "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
     }
+    
 }
+
